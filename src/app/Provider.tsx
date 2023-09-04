506d4cc00715ffbb92bb3b7e5fc498efe177021d
Hun5LEE <jhlee091697@gmail.com>
2023-09-04 17:30:38 +0900

    m

---
+++ src/app/Provider.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import store from "@/store/store";
+import { ReactNode, useState } from "react";
+import { Provider as ReduxProvider } from "react-redux";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+// import "sfac-designkit-react/style.css";
+
+const Provider = ({ children }: { children: ReactNode }) => {
+  const [queryClient] = useState(new QueryClient());
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <ReduxProvider store={store}>{children}</ReduxProvider>
+    </QueryClientProvider>
+  );
+};
+
+export default Provider;
