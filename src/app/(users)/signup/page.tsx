0fe290faca367afd771b840659f176b830c3ce80
jungyeonwoo <oscar0421@kakao.com>
2023-10-12 19:00:08 +0900

    Feat: login, signup router 유효성검사 로직 추가 및 코드 정리

--- src/app/(users)/signup/page.tsx
+++ src/app/(users)/signup/page.tsx
@@ -1,49 +1,80 @@
 "use client";
-import Button from "@/components/users/Button";
-import LinkInfo from "@/components/users/LinkInfo";
-import UserDataInput from "@/components/users/UserDataInput";
-import { postFetch } from "@/utils/fetch/core";
 import React from "react";
+import {
+  Button,
+  SignupForm,
+  UserDataInput,
+  ErrorMessage,
+  LinkInfo,
+} from "@/components/users";
+import {
+  checkEmptyObject,
+  checkPassword,
+  handleSignup,
+  validateEmail,
+  validatePassword,
+} from "@/utils/userUtils";
 import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context";
 
 const Signup = (): JSX.Element => {
   const {
     register,
     handleSubmit,
-    formState: { isSubmitting, errors },
+    setError,
+    clearErrors,
+    watch,
+    formState: { isSubmitting, errors, isValid },
   } = useForm();
+  const router: AppRouterInstance = useRouter();
 
   return (
-    <div className="w-full">
+    <SignupForm>
       <form
-        onSubmit={handleSubmit(async (data) =>
-          postFetch("/users/signup", JSON.stringify(data)),
-        )}
+        onSubmit={handleSubmit((data) => handleSignup(data, router, setError))}
       >
         <UserDataInput
           {...register("email", {
             required: "이메일은 필수 입력입니다.",
+            onChange: (data) => {
+              validateEmail(data.target.value, setError, clearErrors);
+            },
           })}
           isError={!!errors.email}
           type="email"
           placeholder="이메일을 입력해 주세요."
         />
+        <ErrorMessage>{errors.email?.message?.toString()}</ErrorMessage>
         <UserDataInput
           {...register("password", {
-            required: true,
+            required: "비밀번호는 필수 입력입니다.",
+            onChange: (data) => {
+              validatePassword(data.target.value, setError, clearErrors);
+            },
           })}
           isError={!!errors.password}
           type="password"
           placeholder="비밀번호를 입력해 주세요."
         />
+        <ErrorMessage>{errors.password?.message?.toString()}</ErrorMessage>
         <UserDataInput
           {...register("rePassword", {
-            required: true,
+            required: "비밀번호 확인은 필수입니다.",
+            onChange: (data) => {
+              checkPassword(
+                watch("password"),
+                data.target.value,
+                setError,
+                clearErrors,
+              );
+            },
           })}
           isError={!!errors.rePassword}
           type="password"
           placeholder="비밀번호를 다시 입력해 주세요."
         />
+        <ErrorMessage>{errors.rePassword?.message?.toString()}</ErrorMessage>
         <UserDataInput
           {...register("username", {
             required: false,
@@ -53,9 +84,13 @@ const Signup = (): JSX.Element => {
           placeholder="(선택사항) 닉네임을 입력해 주세요."
         />
         <LinkInfo />
-        <Button disabled={isSubmitting}>회원가입</Button>
+        <Button
+          disabled={!isValid || isSubmitting || !checkEmptyObject(errors)}
+        >
+          회원가입
+        </Button>
       </form>
-    </div>
+    </SignupForm>
   );
 };
 
