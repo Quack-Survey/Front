86a12df79d73d67c2c1ec84a456f3bed54e9c35e
Doni <nanhimang@naver.com>
2023-10-24 21:28:19 +0900

    Refactor: key props 설정

--- src/components/dashboard/result/DashboardDescription.tsx
+++ src/components/dashboard/result/DashboardDescription.tsx
@@ -7,9 +7,9 @@ const DashboardDescription = ({
 }: IDashboardDescriptionProps): JSX.Element => {
   return (
     <div className="flex max-h-[200px] w-full flex-col gap-n-xs overflow-scroll">
-      {descriptionData.map((desc) => (
+      {descriptionData.map((desc, index) => (
         <div
-          key={desc}
+          key={index}
           className="w-full rounded-n-sm bg-n-light-gray px-n-md py-n-xs text-n-sm"
         >
           {desc}
